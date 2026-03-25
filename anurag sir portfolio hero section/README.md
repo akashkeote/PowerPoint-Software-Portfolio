2a55d6e06ac9b008b36f7b2f98f1b9b778eb7577
Total Overdose <keoteakash@gmail.com>
2026-03-25 14:53:07 +0530

    after cleanup

--- anurag sir portfolio hero section/README.md
+++
@@ -1,3 +0,0 @@
-# updated-portfolio
-##Added a new line.
-Add another new line.
--- patch_include.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let file = 'partials/ribbon-toolbar.html';
-let c = fs.readFileSync(file, 'utf8');
-c = c.replace(/<include src="\.\/cta-group\.html"><\/include>/g, '<include src="./partials/cta-group.html" />');
-fs.writeFileSync(file, c);
-console.log("Patched includes successfully!");

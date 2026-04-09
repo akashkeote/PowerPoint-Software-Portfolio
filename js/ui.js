1f25388f5d0cc60336daa14fbd46110f4f0ad45a
Total Overdose <keoteakash@gmail.com>
2026-04-09 18:02:36 +0530

    before implementation of SEO

--- js/ui.js
+++ js/ui.js
@@ -68,13 +68,13 @@
     document.getElementById('status-present-btn').addEventListener('click', startPresentation);
     document.querySelectorAll('.download-engineering-notes-btn').forEach(btn => {
       btn.addEventListener('click', () => {
-        window.open('https://drive.google.com/drive/folders/14gwS2GpMkEbUhupOc-_rJtEkGLhwNldQ', '_blank', 'noopener');
+        window.open('https://drive.google.com/drive/folders/14gwS2GpMkEbUhupOc-_rJtEkGLhwNldQ', '_blank', 'noopener,noreferrer');
       });
     });
 
     document.querySelectorAll('.download-resume-btn').forEach(btn => {
       btn.addEventListener('click', () => {
-        window.open('https://drive.google.com/drive/folders/1ID1uHzPu7DBdMX9e4SsYGLmeYbclXA76?usp=drive_link', '_blank', 'noopener');
+        window.open('https://drive.google.com/drive/folders/1ID1uHzPu7DBdMX9e4SsYGLmeYbclXA76?usp=drive_link', '_blank', 'noopener,noreferrer');
       });
     });
 
